2868df5396db9f4bfcddae8cd10f56520cf1bff3
sam49877 <61464193+sam49877@users.noreply.github.com>
2026-03-08 15:05:46 +0530

    Ok button removed after the entry

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbx9hoQWLC77KOaxO__sZu20qBjDhu-tn64ps-491p-uuqd5dmsKK2AvOWSTiCVX6xKJ/exec";
 
+
 function selectButton(button,type){
 
 let buttons = button.parentElement.querySelectorAll("button");
 
 buttons.forEach(btn=>{
-btn.classList.remove("active")
+btn.classList.remove("active");
 });
 
 button.classList.add("active");
@@ -14,9 +15,11 @@ document.getElementById(type).value = button.innerText;
 
 }
 
+
 function generateTxnId(){
 return Math.floor(Math.random()*100000);
 }
+
 
 function submitData(){
 
@@ -38,11 +41,38 @@ tags:document.getElementById("tags").value
 
 };
 
+
 fetch(scriptURL,{
 method:"POST",
 body:JSON.stringify(data)
 })
-.then(response => alert("Entry Added Successfully"))
-.catch(error => alert("Error adding entry"));
+.then(response => {
+
+clearForm();
+
+})
+.catch(error => {
+
+console.log("Error adding entry");
+
+});
+
+}
+
+
+function clearForm(){
+
+document.getElementById("description").value="";
+document.getElementById("subcategory").value="";
+document.getElementById("amount").value="";
+document.getElementById("tags").value="";
+
+document.querySelectorAll(".button-group button").forEach(btn=>{
+btn.classList.remove("active");
+});
+
+document.querySelectorAll("input[type=hidden]").forEach(input=>{
+input.value="";
+});
 
 }
