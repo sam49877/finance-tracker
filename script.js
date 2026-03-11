0c31a6ca1647d4293e4c7f23d6d9094034cac057
sam49877 <61464193+sam49877@users.noreply.github.com>
2026-03-11 14:57:33 +0530

    Adding analytics Page and Trigger

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbx9hoQWLC77KOaxO__sZu20qBjDhu-tn64ps-491p-uuqd5dmsKK2AvOWSTiCVX6xKJ/exec";
 
-
 function selectButton(button,type){
 
-let buttons = button.parentElement.querySelectorAll("button");
+let buttons = button.parentElement.querySelectorAll("button")
 
 buttons.forEach(btn=>{
-btn.classList.remove("active");
-});
+btn.classList.remove("active")
+})
 
-button.classList.add("active");
+button.classList.add("active")
 
-document.getElementById(type).value = button.innerText;
+document.getElementById(type).value = button.innerText
 
 }
-
 
 function generateTxnId(){
-return Math.floor(Math.random()*100000);
-}
 
+return Math.floor(Math.random()*100000)
+
+}
 
 function submitData(){
 
@@ -27,52 +26,27 @@ let data = {
 
 id:generateTxnId(),
 date:document.getElementById("date").value,
+month:document.getElementById("month").value,
 type:"Expense",
 category:document.getElementById("category").value,
 subcategory:document.getElementById("subcategory").value,
 description:document.getElementById("description").value,
 merchant:document.getElementById("merchant").value,
 paymentmode:document.getElementById("paymentmode").value,
+source:document.getElementById("source").value,
 frequency:document.getElementById("frequency").value,
 fixedvariable:document.getElementById("fixedvariable").value,
 needwant:document.getElementById("needwant").value,
 amount:document.getElementById("amount").value,
 tags:document.getElementById("tags").value
 
-};
-
+}
 
 fetch(scriptURL,{
 method:"POST",
 body:JSON.stringify(data)
 })
-.then(response => {
-
-clearForm();
-
-})
-.catch(error => {
-
-console.log("Error adding entry");
-
-});
-
-}
-
-
-function clearForm(){
-
-document.getElementById("description").value="";
-document.getElementById("subcategory").value="";
-document.getElementById("amount").value="";
-document.getElementById("tags").value="";
-
-document.querySelectorAll(".button-group button").forEach(btn=>{
-btn.classList.remove("active");
-});
-
-document.querySelectorAll("input[type=hidden]").forEach(input=>{
-input.value="";
-});
+.then(response => alert("Entry Added Successfully"))
+.catch(error => alert("Error adding entry"))
 
 }
